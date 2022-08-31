fc3c949dd82916acd069da6505a23dae6a60f455
omarkhodjaev <umarkhankhodjaev.com>
2022-08-31 20:49:51 +0500

    exam

---
+++ main.js
@@ -0,0 +1,31 @@
+// 1)
+
+
+// var arr = ["I", "Study", "Javscript" , "right", "now"];
+// let arr1 = arr.splice(2,1)
+// console.log(arr)
+
+// ________________________________________________________________________________
+
+
+
+// // 3) 
+// function myCheck(num){
+//     if(num % 2 == 0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// console.log(myCheck(3))
+
+// _____________________________________________________________________________________
+
+// 2)
+
+let nom = "Salom dunyo"
+
+console.log(nom.split(` `).reverse())
+
